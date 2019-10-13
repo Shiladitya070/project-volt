f279bd159d354b5aafd3dee8a371051fc5152520
Immortal <shiladityad940@gmail.com>
2019-10-13 15:56:13 +0530

    updated settings

--- hike/settings.py
+++ hike/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!s48_2(mus7si^*0i$+yenrv5cp(_3#c&u824hc-_0)mr71)yd'
+# SECRET_KEY = '!s48_2(mus7si^*0i$+yenrv5cp(_3#c&u824hc-_0)mr71)yd'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hipe101.herokuapp.com']
 
 
 # Application definition
@@ -136,6 +137,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "shiladityad940@gmail.com"
-EMAIL_HOST_PASSWORD = "pqpilwryuqgtbgtd"
+EMAIL_HOST_USER = os.environ.get("user")
+EMAIL_HOST_PASSWORD = os.environ.get("smtp_pass")
 
---
+++ test.py
@@ -0,0 +1,5 @@
+import os
+a = os.environ.get("SECRET_KEY")
+b = os.environ.get("smtp_pass")
+print(a)
+print(b)
