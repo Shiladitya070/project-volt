c61183e872d6f38a0632ef81ba15a05cfac20651
Immortal <shiladityad940@gmail.com>
2019-10-13 14:44:29 +0530

    Initial commit

--- blog/views.py
+++ blog/views.py
@@ -34,7 +34,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'content',]
 
     def form_valid(self, form):
         form.instance.author = self.request.user
--- users/models.py
+++ users/models.py
@@ -10,8 +10,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} profile'
 
-    def save(self):
-        super().save()
+    def save(self,*args,**kwargs):
+        super().save(*args, **kwargs)
     
         img = Image.open(self.image.path)
 
