d6f728802398e558a20c89ad5b33b57977d4a733
Immortal <shiladityad940@gmail.com>
2019-10-13 16:42:36 +0530

    added django-heroku

--- hike/settings.py
+++ hike/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hipe101.herokuapp.com']
+ALLOWED_HOSTS = ['hipe101.herokuapp.com','localhost']
 
 
 # Application definition
@@ -139,4 +140,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("user")
 EMAIL_HOST_PASSWORD = os.environ.get("smtp_pass")
+
+
+django_heroku.settings(locals())
 
