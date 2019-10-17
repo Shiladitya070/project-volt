6f21c3beb52fa41ff521a757c01deb7f65b6930e
Immortal <shiladityad940@gmail.com>
2019-10-17 13:14:43 +0530

    date

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,8 @@ from django.urls import reverse
 class Post (models.Model):
     title = models.CharField(max_length = 100 )
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.datetime.now())
-    print(date_posted)
+    date_posted = models.DateTimeField(default=timezone.now())
+    # print(date_posted)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
 
     def __str__(self):
