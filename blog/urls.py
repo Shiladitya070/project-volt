458bcb3779f9ed6f00cf9f793710d8cdf6f3878b
Antar Mukhopadhaya <37949912+arun199941@users.noreply.github.com>
2021-10-31 21:03:18 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import PostListView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
+from .views import post_list_view, PostCreateView, PostUpdateView, PostDeleteView, user_post_list_view
+
 
 urlpatterns = [
     path('', post_list_view.as_view(), name='blog-home'),
     path('user/<str:username>', user_post_list_view.as_view(), name='user-posts'),
     path('profile/<str:username>', views.get_user_profile, name='user-profile'),
-    # path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('post/<slug:slug>/', views.detail, name='post-detail'),
     path('update/<slug:slug>', views.updatePost, name="update"),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
